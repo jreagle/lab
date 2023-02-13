13a172154bc64f01b88c15f1d0585d80bc926e9c
Joseph Reagle <109813833+jreagle@users.noreply.github.com>
2023-02-13 19:05:25 -0300

    Update postprocess.ts

--- postprocess.ts
+++ postprocess.ts
@@ -3,6 +3,6 @@ import { unZipFromFile, unZipFromURL } from 'https://deno.land/x/flat@0.0.15/mod
 // zip from a local file
 // (zip file, destination path)
 const filename = Deno.args[0]
-const result = await unZipFromFile(filename, './data/2023/csv')
+const result = await unZipFromFile(filename, './')
 const output = result ? 'File unzipped successfully' : 'Error unzipping'
 console.log(output)
